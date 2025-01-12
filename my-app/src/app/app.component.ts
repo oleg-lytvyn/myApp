88ec0ab3b997bc95805d55c07fafd1177b780fe7
Oleh <olehlytvyn.personal@gmail.com>
2025-01-12 17:19:13 +0000

    Prepare for update

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet, ProductListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'NG';
+  title = 'World';
 }
